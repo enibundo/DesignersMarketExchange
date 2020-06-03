b36816900f097458a0bc85ae8b60b87f78857a53
Eni BUNDO <eni.bundo@gmail.com>
2020-06-03 12:00:12 +0200

    changes in deliver

--- IndependentIllustratorsExchange.sol
+++ IndependentIllustratorsExchange.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.6.0;
+pragma solidity ^0.6.8;
 
 pragma experimental ABIEncoderV2;
 
@@ -15,7 +15,9 @@ contract IndependentIllustratorsExchange
     }
     
     enum RequestState { Default, Open, Ongoing, Closed }
-        
+    
+    enum SubscriptionType { Illustrator, Requester } 
+    
     struct Request 
     {
         RequestState State;
@@ -24,8 +26,9 @@ contract IndependentIllustratorsExchange
         uint AcceptDelaySeconds;
         string Description;
         uint MinReputation;
-        Illustrator[] Applications;
+        address[] Applications;
         address ChosenApplication;
+        bytes32 Artifact;
     }
     
     mapping (address => Illustrator) public Illustrators;
@@ -33,10 +36,14 @@ contract IndependentIllustratorsExchange
     
     Request[] public Requests;
     
-    function Subscribe(string memory illustratorName) public
+    function Subscribe(string memory illustratorName, SubscriptionType subscriptionType) public
     {
         require(Subscriptions[msg.sender] == false, "Already subscribed.");
-        Illustrators[msg.sender] = Illustrator(1, illustratorName);
+        
+        if (subscriptionType == SubscriptionType.Illustrator)
+            Illustrators[msg.sender] = Illustrator(1, illustratorName);
+        
+        Subscriptions[msg.sender] = true;
     }
     
    function CreateRequest(uint acceptDelaySeconds, string memory description, uint minReputation) payable public
@@ -44,7 +51,7 @@ contract IndependentIllustratorsExchange
        require (Subscriptions[msg.sender] == true, "Should be subscribed before creation request");
        
        // 2% fee
-       uint paymentInWei = SafeMath.div(msg.value, uint(1.02));
+       uint paymentInWei = SafeMath.div(SafeMath.mul(msg.value, 2), 100);
        
        Request memory request = Request(RequestState.Open, 
                                         msg.sender, 
@@ -52,36 +59,42 @@ contract IndependentIllustratorsExchange
                                         acceptDelaySeconds, 
                                         description, 
                                         minReputation, 
-                                        new Illustrator[](0), 
-                                        address(0));
+                                        new address[](0), 
+                                        address(0),
+                                        bytes32(0));
        Requests.push(request);
    }
    
    function ApplyToRequest(uint requestId) public
    {
-       require(Requests[requestId].State == RequestState.Open, "Should be open request");
+       require(Subscriptions[msg.sender] == true, "You should be subscribed");
        require(Illustrators[msg.sender].Reputation >= Requests[requestId].MinReputation, "Not enough reputation");
+       require(Requests[requestId].State == RequestState.Open, "Should be open request");
        
-       Requests[requestId].Applications.push(Illustrators[msg.sender]);
+       Requests[requestId].Applications.push(msg.sender);
    }
    
    function AcceptApplication(uint requestId, address illustratorsAddress) public
    {
        require(Requests[requestId].Requester == msg.sender, "Cannot accept application, not request creator");
+       
        Requests[requestId].State = RequestState.Ongoing;
        Requests[requestId].ChosenApplication = illustratorsAddress;
    }
    
-   function GetRequests() public returns (Request[] memory)
+   function GetRequests() public view returns (Request[] memory)
    {
        return Requests;
    }
    
-   function Deliver(uint requestId) public 
+   function Deliver(uint requestId, bytes32 artifact) public 
    {
        require (Illustrators[msg.sender].Reputation > 0, "Should be a registered Illustrator");
        require (Requests[requestId].ChosenApplication == msg.sender, "You are not the chosen application");
        
-       msg.sender.send(Requests[requestId].PaymentWei);
+       Illustrators[msg.sender].Reputation = SafeMath.add(Illustrators[msg.sender].Reputation, 1); 
+       msg.sender.transfer(Requests[requestId].PaymentWei);
+       Requests[requestId].State = RequestState.Closed;
+       Requests[requestId].Artifact = artifact;
    }
 }
